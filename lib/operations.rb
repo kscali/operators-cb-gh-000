807f9d30775ac62690c98d7507a6ecd772cdc58a
Koy Saeteurn <kosatun@aol.com>
2018-07-25 06:34:28 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-  speed < 30 || speed > 90 ? true : false
+  if (speed < 30 || speed > 60)
+    true
+  else
+    false
+  end    
 end
 
 
 
 def not_safe?(speed)
-  	
+  speed < 30 || speed > 60 ? true : false	
 end
 	
 
